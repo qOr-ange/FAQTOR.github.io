339204f117176a2e62dae8fff3bf4bd53a3ff94c
qOr-ange <metsgge@gmail.com>
2024-10-24 10:21:01 +0300

    initial commit

---
+++ js/resLoader.js
@@ -0,0 +1,34 @@
+﻿const resources = [
+   { file: 'faq/faq0.html', elementId: 'WhatLanguage' },
+   { file: 'faq/faq1.html', elementId: 'WhatToMake' },
+   { file: 'faq/faq2.html', elementId: 'StayMotivated' },
+   { file: 'faq/faq3.html', elementId: 'WorkWithOthers' },
+   { file: 'faq/faq4.html', elementId: 'IHaveThisCodePleaseHelp' },
+   { file: 'faq/faq5.html', elementId: 'IWantToLearnXYZ' },
+   { file: 'res/background.html', elementId: 'background-ascii' },
+   { file: 'res/logo.html', elementId: 'logo' }
+];
+
+const loadResource = async (file, elementId) => {
+   try {
+      const response = await fetch(file);
+      if (!response.ok) {
+         throw new Error(`Failed to load ${file}: ${response.status}`);
+      }
+      const html = await response.text();
+      document.getElementById(elementId).innerHTML = html;
+   } catch (error) {
+      console.error(error);
+   }
+};
+
+const loadResources = async () => {
+   await Promise.all(resources.map(({ file, elementId }) => {
+      console.log(`Loading resource: ${file} into element ID: ${elementId}`);
+      return loadResource(file, elementId);
+   }));
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+   loadResources();
+});
